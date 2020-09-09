8ebca423cff6116fe86f0258f6f04d86699e637a
Dmitry Gutoryan <dmitry.gutoryan@gmail.com>
2020-09-09 16:46:36 +0300

    chore: remove initial extra code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'dg-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dgutoryan';
-}
+export class AppComponent {}
