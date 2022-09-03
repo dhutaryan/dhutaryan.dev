7a3d8cc39487f621374cff0b01b2b457c02643dd
Dmitry Gutoryan <dmitry.gutoryan@gmail.com>
2022-09-04 00:06:12 +0300

    refactor: clean up the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,9 @@ import { FlexModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CvLayoutComponent } from './cv-layout/cv-layout.component';
-import { GeneralInfoComponent } from './general-info/general-info.component';
-import { ExperienceInfoComponent } from './experience-info/experience-info.component';
-import { SummaryInfoComponent } from './summary-info/summary-info.component';
-import { SectionHeaderComponent } from './section-header/section-header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CvLayoutComponent,
-    GeneralInfoComponent,
-    ExperienceInfoComponent,
-    SummaryInfoComponent,
-    SectionHeaderComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, FlexModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/experience-info/experience-info.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-  selector: 'dg-experience-info',
-  templateUrl: './experience-info.component.html',
-  styleUrls: ['./experience-info.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class ExperienceInfoComponent {}
